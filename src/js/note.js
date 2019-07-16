afc3d7942f413db5b2346f508703a238dacfc58c
Gabriela Kostova <kostovagm@gmail.com>
2019-07-15 21:47:35 -0400

    note branch added

---
+++ src/js/note.js
@@ -0,0 +1,161 @@
+const note = [
+  {
+    note_id: 1,
+    user_id: 1,
+    title: 'Javascript workshop', 
+    content: 'JavaScript was initially created to make web pages alive. The programs in this language are called scripts. They can be written right in a web page’s HTML and run automatically as the page loads. Scripts are provided and executed as plain text. They don’t need special preparation or compilation to run. In this aspect, JavaScript is very different from another language called Java.',
+    dateCreated: `04/02/2019`,
+    dateModified: `05/02/2019`,
+    tag_id: [1, 2, 3],
+  },
+
+  {
+    note_id: 2,
+    user_id: 1,
+    title: 'Interface dev 1 class notes', 
+    content: 'Usually, the javascript file is called script.js and placed in a folder called js.'\n'Apply javascript as a <script src> and put as the last thing in the body to ensure the browser reads all the content before executing the javascript code e.g., <script src="myscripts.js"></script>.\nIf we have multiple js files attached, just attach them in chronological order in the html; e.g., data file first and then function file.\n',
+    dateCreated: `05/15/2019`,
+    dateModified: `06/13/2019`,
+    tag_id: [1, 4, 5],
+  },
+
+  {
+    note_id: 3,
+    user_id: 1,
+    title: 'Html class 6 May 2019', 
+    content: 'HTML AND CSS are used for website presentation. Javascript is actually web version of actual programming code, meant to write applets for use on those websites. JavaScript is used to change the behavior and dynamic content of the website. HTML5 is used to prepare text and hence that it can be displayed on web-browser.',
+    dateCreated: `01/21/2019`,
+    dateModified: `03/21/2019`,
+    tag_id: [1, 4, 5, 6, 7],
+  },
+
+  {
+    note_id: 4,
+    user_id: 1,
+    title: 'Shopping list', 
+    content: 'Eggs, milk, sugar, bananas, hazelnuts',
+    dateCreated: `01/12/2018`,
+    dateModified: `01/12/2019`,
+    tag_id: [],
+  },
+
+
+  {
+    note_id: 5,
+    user_id: 2,
+    title: 'assignment of Javascript', 
+    content: 'create data set in gorups and write it in VScode',
+    dateCreated: `13/07/2019`,
+    dateModified: `13/07/2019`,
+    tag_id: [9],
+  },
+
+
+  {
+    note_id: 6,
+    user_id: 2,
+    title: 'important date of school', 
+    content: 'program register date is july 17,2019',
+    dateCreated: `07/06/2019`,
+    dateModified: `07/06/2019`,
+    tag_id: [10],
+  },
+
+
+  {
+    note_id: 7,
+    user_id: 2,
+    title: 'how to create outline in Photoshop', 
+    content: 'use the pen tool in PS, follow the reference picture to draw the outline. You could drag and draw to make the curve line. To redo the step, you could go to history to delect the step you do not want anymore.',
+    dateCreated: `15/02/2019`,
+    dateModified: `15/02/2019`,
+    tag_id: [8],
+  },
+
+
+  {
+    note_id: 8,
+    user_id: 2,
+    title: 'create gradient colors in Adobe Illustrator', 
+    content: 'In the AI, use the gradient tool to get the gradient bar. Click on the bar to create more colors when you need it. You could change the color by clicking in the color box. If you want to delect the color, just darg the color box out of the gradient bar.',
+    dateCreated: `20/11/2018`,
+    dateModified: `20/11/2018`,
+    tag_id: [11],
+  },
+
+
+  {
+    note_id: 9,
+    user_id: 3,
+    title: 'acute inflammation', 
+    content: 'glomerulonephritis\n\nAcute inflammation of the kidney.\n\nAcute: fast onset. \n\nCauses blockages that could have contributed to pulmonary edema\n\n￼\nEnd stage renal failure\n\nLast Stage of chronic kidney disease.\n\nChronic:developed over a long period of time\n\nThe kidney is no longer able to remove waste from the body\n\nThe lose of the bodies ability to filter to blood results in there be a excess of fluid causing edema\n\nThe tissue also become more acidic because of the waste buildup in the tissue\n\nDecomposition happens more rapidly \n\n\n\nWe used cotton to remove fluid to help remove blood from our incision\n\n',
+    dateCreated: `26/2/2014`,
+    dateModified: `15/4/2028`,
+    tag_id: [12],
+  },
+
+
+  {
+    note_id: 10,
+    user_id: 3,
+    title: 'Dnd infographic', 
+    content: 'Topic:\nIn dungeons and dragons a player is allowed to create there own character using races and classes based on classic fantasy archetypes. These archetypes have there own benefits and weakness helping a person choice the roles and races that best suit who they want their character to be.\n \nThe Questions:\nWhat is the most/least played race?\nWhat is the most/least played class?\nWhat races are usually only played as one or two class type?\nWhat classes tend to only be played by a few Races?\n',
+    dateCreated: `12/06/2017`,
+    dateModified: `12/06/2017`,
+    tag_id: [14],
+  },
+
+
+  {
+    note_id: 11,
+    user_id: 3,
+    title: 'Pre-injection', 
+    content: 'pre-injection: prior to introduction arterial fluid into the vascular system. one \n\nwater corrective:1st chemical in the tank2 ounces per galloon, always used, relieves surface tension.\n\ndrainage chemical: second thing added to the tank.helps rehydrate, dilates blood vessels, changes ph in tissues,helps wash away the effect of drugs. us with co injection though pre is always better. 1:1 up to 32oz.\n\narterial fluid: HCHO main disinfectant and preservative. 3rd chem added, tints, humectants, contains disinfectants, index amount + amount of HCHO (18-36)\ncavity fluid: after arterial injection injecte into the ventral cavity, index % low 8-24, drys out  organs, want to use everytime, edema dilutes, rain onto organs,proper amounts helps dehydration tissues\n',
+    dateCreated: `15/02/2013`,
+    dateModified: `15/02/2013`,
+    tag_id: [12],
+  },
+
+
+  {
+    note_id: 12,
+    user_id: 3,
+    title: 'Working as a Director', 
+    content: 'Working as a Director \n\nThe funeral directors job is broken up into 3 parts which are: arranging, directing, and embalming. During an arrangement, the director will meet with the loved ones of the deceased to plan the funeral and go over the services that they will be providing. During this time, contracts are made and different merchandise are shown to the family. This of course, happens after the first call is made to the funeral home. The first call is when the family phones the funeral home asking about services. After this phone call, information about the deceased and their family is recorded and a plan of action is created.\n',
+    dateCreated: `14/10/2015`,
+    dateModified: `18/07/2018`,
+    tag_id: [12],
+  },
+
+
+  {
+    note_id: 13,
+    user_id: 3,
+    title: 'Don’t use your Heads Kids', 
+    content: 'Childrens safety is always a complicated issue and the author explains in the article that there is a double standard between young boys and girls. This results in writer being very careful with  regards to the different sex’s because its hard for the author as a male to touch on an issue that females have in society.  \n\nThe article was trying to explain how unsafe head balling is and it uses the way that one sex is allowed to do it and the other is not because it is unsafe as an example of why it should not be allowed. The article looks like it was made for an adult audience who are parents of the own kids. He talks a lot about his experience as a coach and the kids he coaches a lot of it things only parents can truly understand.  You cannot relate to whether the sport is worth the risk to your child if you are not a parent who live with the fear of losing their children. The author knows this and he writes this article with relatable experiences for parents so they can get a clearer image of what he means.\n',
+    dateCreated: `12/09/2016`,
+    dateModified: `12/09/2016`,
+    tag_id: [],
+  },
+
+
+  {
+    note_id: 14,
+    user_id: 3,
+    title: 'Purge Prevention', 
+    content: 'pre embalming\ncream lips after cleaning\nuse nasal aspirator\ndisinfect\nset feature\nligation or packing trachea if absolutely necessary\nembalming\nif minor and not disrupting circulation cream the face\nonce complete, reclean and repack mouth\nstomach\nusually gastro-intestinal\nhydrochlic acid from stomach is most dehydrating and distorting\nlung\ncommen with phneumonia, drowning, asphyxia, CO poisoning, accidents where chest was crushed\nbrain\nembalming may mean decomp. in brain, infections or trauma to head\nlook for distended eyes for sign\nit will have a odourless white cream consistency\ncoffee grounds\ngastro intestinal tract\nresult of blood mixing with stomach acid\n10 ways to preven purges\navoid excessive massage of adbomen\navoid delaying drainage too long\navoid excessive pressure\navoid excessive rate of flow\navoid poor injection\navoid poor drainage\navoid poor aspiration\navoid poor placement of cavity fluid\ndont tighten belt too much\nkeep head up and hips down\n',
+    dateCreated: `18/03/2016`,
+    dateModified: `12/06/2017`,
+    tag_id: [12, 13],
+  },
+
+  {
+    note_id: 15,
+    user_id: 3,
+    title: 'Cellular Injury', 
+    content: '\nCELLULAR INJURY\n\nCauses of cell injury\n\n1. Hypoxia due to ischemia (low oxygen) (most common cause)\n(hypo means low, hyper means high)\n\n2. physical agents (mechanical trauma, temperature extremes, radiation, electric shock)\n\n3. chemical agents and drugs (acids\n\n4.infectious agents\n\n5.imminulogic reactions \n6.genetic derangements\n7.nutritional imbalances\n8.aging (cellular senescence), \n\nWhat’s going to get you first?\n\n                                 Cellular injury\nacute                                                         chronic\n(quick onset, potentially curable)              (ongoing)\n\nAcute cellular injuries:\nSub-lethal or Lethal (lack of oxygen, cell starts to swell)\n\nCytoplasmic changes: \ninflux of H20 (swelling)\nblebs may form on surface\nmitochondria swell\nsome lymosomes release enzymes\n\nATP is the energy source of the cell. It pumps ions across the cell membrane. Hypoxia (a lack of oxygen in cell, makes atp unable to produce, this leads to swelling as more water comes in)\n\nLysozomes will release enzymes, eating the cell\n\nLethal Acute Cell injury:\n\nUp to a certain point, the above changes are reversable \ncellular sweilling -> disruption of organelles\nLysozomes release enzymes\n\nmost cells die by apoptosis (programmed death), but when a cell can’t produce enough ATP it’s a messy death. Leaves behind cellular debris. These cells weren’t ready to die!\n\nCytoplasmic changes:\nNucleus (DNA) degenerates\nextensive damage to plasma membrane\nlysozomes rupture, release enzymes\nthese changes are irreversable\nthese changes cause cell death (necrosis, oh no-sis!)\n',
+    dateCreated: `18/03/2012`,
+    dateModified: `18/03/2014`,
+    tag_id: [13],
+  },
+];
