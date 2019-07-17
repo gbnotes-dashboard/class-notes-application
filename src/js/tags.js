81ed4e565e9e2adf85be3c3cb38ab64da23c96fd
DamianSpaziani <damian.spaziani@gmail.com>
2019-07-16 22:30:41 -0400

    Tags added

---
+++ src/js/tags.js
@@ -0,0 +1,13 @@
+const tags = [
+  {userFirstName: 'Gabby', userLastName: 'Kostova', userEmail: 'kostovagm@gmail.com', tags1: [{tag: 'JavaScript', favourite: true, color: `#FF0000`}, {tagName: 'Workshop', favourite: false}, {tagName: 'Conference', favourite: false, color: `#F4D03F`}, {tagName: 'gbc', favourite: false},{tagName: 'Semester 2', favourite: false, color: `#3498DB`}, {tagName: 'html', favourite: true}, {tagName: 'css', favourite: true}
+] }, 
+
+  {userFirstName: 'Leah', userLastName: 'Chow', userEmail: 'ctzhouleya@gmail.com', tags2: [{tag: 'Photoshop', favourite: false}, {tagName: 'Javascript', favourite: false}, {tagName: 'School Information', favourite: true, color: `#FF0000`}, {tagName: 'Illustrator', favourite: false}
+] }, 
+
+  {userFirstName: 'Damian', userLastName: 'Spaziani', userEmail: 'damian.spaziani22@gmail.com', tags3: [{tag: 'Theory Embalming', favourite: false, color: `#28B463`}, {tagName: 'Embalming Practical', favourite: false}, {tagName: 'Illustrator', favourite: true}
+] } ]
+
+const collection = [
+  {}
+]
