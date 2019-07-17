4f207af413520ab29d5a03d39e2fbf717808ede1
Graphic <graphic@WFD302-24m.local>
2019-07-17 08:30:33 -0400

    changed tag names

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,11 +1,11 @@
 const tags = [
-  {userFirstName: 'Gabby', userLastName: 'Kostova', userEmail: 'kostovagm@gmail.com', tags1: [{tag: 'JavaScript', favourite: true, color: `#FF0000`}, {tagName: 'Workshop', favourite: false}, {tagName: 'Conference', favourite: false, color: `#F4D03F`}, {tagName: 'gbc', favourite: false},{tagName: 'Semester 2', favourite: false, color: `#3498DB`}, {tagName: 'html', favourite: true}, {tagName: 'css', favourite: true}
+  {userFirstName: 'Gabby', userLastName: 'Kostova', userEmail: 'kostovagm@gmail.com', userTags: [{tagName: 'JavaScript', favourite: true, color: `#FF0000`}, {tagName: 'Workshop', favourite: false}, {tagName: 'Conference', favourite: false, color: `#F4D03F`}, {tagName: 'gbc', favourite: false},{tagName: 'Semester 2', favourite: false, color: `#3498DB`}, {tagName: 'html', favourite: true}, {tagName: 'css', favourite: true}
 ] }, 
 
-  {userFirstName: 'Leah', userLastName: 'Chow', userEmail: 'ctzhouleya@gmail.com', tags2: [{tag: 'Photoshop', favourite: false}, {tagName: 'Javascript', favourite: false}, {tagName: 'School Information', favourite: true, color: `#FF0000`}, {tagName: 'Illustrator', favourite: false}
+  {userFirstName: 'Leah', userLastName: 'Chow', userEmail: 'ctzhouleya@gmail.com', userTags: [{tagName: 'Photoshop', favourite: false}, {tagName: 'Javascript', favourite: false}, {tagName: 'School Information', favourite: true, color: `#FF0000`}, {tagName: 'Illustrator', favourite: false}
 ] }, 
 
-  {userFirstName: 'Damian', userLastName: 'Spaziani', userEmail: 'damian.spaziani22@gmail.com', tags3: [{tag: 'Theory Embalming', favourite: false, color: `#28B463`}, {tagName: 'Embalming Practical', favourite: false}, {tagName: 'Illustrator', favourite: true}
+  {userFirstName: 'Damian', userLastName: 'Spaziani', userEmail: 'damian.spaziani22@gmail.com', userTags: [{tagName: 'Theory Embalming', favourite: false, color: `#28B463`}, {tagName: 'Embalming Practical', favourite: false}, {tagName: 'Illustrator', favourite: true}
 ] } ]
 
 const collection = [
